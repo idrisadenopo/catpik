4cb6d0199749e1860348ab66df0e482e913c00fe
Idris Adenopo <idris.adenopo@gmail.com>
2023-04-11 02:08:55 +0100

    feat: prompt user to refresh cats

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
         'catpik-tertiary-dark': '#3b82f6',
         'catpik-text-dark': '#d4d4d4',
       },
+      keyframes:{
+         shake: {
+          '0%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(5px)' },
+          '50%': { transform: 'translateX(-5px)' },
+          '75%': { transform: 'translateX(5px)' },
+          '100%': { transform: 'translateX(0)' },
+        }
+      },
     },
   },
   plugins: [],
